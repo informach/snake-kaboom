a1ae69e3559fa8ba9be92a2a170689cf953e7743
juliocofelipe <102262011+juliocofelipe@users.noreply.github.com>
2022-07-27 12:10:10 +0000

    Initial commit

---
+++ code/main.js
@@ -0,0 +1,44 @@
+import kaboom from "kaboom"
+
+kaboom()
+
+const directions = {
+  UP: "up",
+  DOWN: "down",
+  LEFT: "left",
+  RIGHT: "right"
+};
+
+let current_direction = directions.RIGHT;
+let run_action = false;
+let snake_length = 3;
+let snake_body = [];
+
+const block_size = 20;
+
+const map = addLevel([
+  "==============",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "=            = ",
+  "==============",
+], {
+  width: block_size,
+  height: block_size,
+  pos: vec2(0, 0),
+  "=": () => [
+    rect(block_size, block_size),
+    color(255, 0, 0),
+    area(),
+    "wall"
+  ]
+})
